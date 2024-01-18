ca8f5922b7d9f6dfb07bb8ac42595b2a475572ed
nichem <nichem9911@qq.com>
2024-01-18 16:26:15 +0800

    移除旧代码

--- app/src/main/java/com/example/reciteword/ui/pages/AppDestinations.kt
+++
@@ -1,7 +0,0 @@
-package com.example.reciteword.ui.pages
-
-object AppDestinations {
-    const val SPLASH_PAGE_ROUTE = "splash_page_route"
-    const val HOME_PAGE_ROUTE = "home_page_route"
-    const val SETTING_PAGE_ROUTE = "setting_page_route"
-}
